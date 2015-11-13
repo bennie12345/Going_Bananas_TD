51e00a79a8d07d5887684cb68b2b1c1648d06cf8
bennie12345 <bmuijldert@gmail.com>
2015-11-13 11:09:49 +0100

    Added temporary background and upgrademenu instantiates at tower position(triggered on play for now)

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject Enemy;
+    [SerializeField]
+    private float timeUntilSpawn = 0f;
+    [SerializeField]
+    private float startTime = 0f;
+    [SerializeField]
+    private float secondsBetweenSpawn = 3f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void SpawnEnemy()
+    {
+
+        GameObject myEnemy = Instantiate(Enemy) as GameObject;
+
+        myEnemy.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timeUntilSpawn = Time.time - startTime;
+
+        if (timeUntilSpawn >= secondsBetweenSpawn)
+        {
+            startTime = Time.time;
+            timeUntilSpawn = 0;
+            SpawnEnemy();
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnTower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnTower : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject tower;
+    [SerializeField]private GameObject upgradeMenu;
+    private GameObject localUpgradeMenu;
+
+	// Use this for initialization
+	void Start () {
+        localUpgradeMenu = (GameObject)Instantiate(upgradeMenu, tower.transform.position, Quaternion.identity);
+        GameObject canvas = GameObject.Find("Canvas");
+        localUpgradeMenu.transform.SetParent(canvas.transform, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            
+            GameObject obj = Instantiate(upgradeMenu, tower.transform.position, Quaternion.identity) as GameObject;
+            obj.transform.SetParent(this.transform);
+        }
+        Debug.Log(tempObject.GetComponent<RectTransform>());
+        localUpgradeMenu.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(tower.transform.position);
+	}
+}
