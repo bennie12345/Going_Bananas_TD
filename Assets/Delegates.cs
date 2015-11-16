bf3a010d9b888486c83db4225bcabf53f0c36bb3
DenzelXIII <denzel_justin@hotmail.com>
2015-11-16 09:01:25 +0100

    worked on the towers

---
+++ Assets/Delegates.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Delegates : MonoBehaviour
+{
+   // delegate void MyDelegate();
+    //MyDelegate myDelegate;
+
+	// Use this for initialization
+	void Start ()
+    {
+        /*myDelegate += ColorChange;
+        myDelegate += ColorChanged;
+
+        myDelegate();*/
+	}
+
+    void OnEnable()
+    {
+        EventManager.OnClicked += ColorChange;
+        EventManager.OnClicked += ColorChanged;
+    }
+
+    void ColorChange()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    void ColorChanged()
+    {
+        print("The color has changed!");
+    }
+}
---
+++ Assets/_scripts/UnitSpawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject unit;
+    [SerializeField]
+    private Transform UnitPos;
+
+    void OnEnable()
+    {
+        EventManager.OnClicked += SpawnUnit;
+    }
+
+    void SpawnUnit()
+    {
+        Instantiate(unit, UnitPos.position, UnitPos.rotation);
+    }
+}
