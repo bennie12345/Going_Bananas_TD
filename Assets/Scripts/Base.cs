5b7af77e53858735dee4b3f1d750b235b6387597
bennie12345 <bmuijldert@gmail.com>
2015-11-17 12:14:40 +0100

    removed some scripts, enemies can now damage the base and get destroyed when they hit it, they also spawn at big intervals, can be changed.

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Base : MonoBehaviour {
+
+    public float baseHealth = 20f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (baseHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            StartCoroutine(DamageBase());
+        }
+    }
+
+    IEnumerator DamageBase()
+    {
+        yield return new WaitForSeconds(1);
+        baseHealth -= 1;
+    }
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,17 +4,33 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour {
 
     [SerializeField]
-    private float moveSpeed;
+    private float _moveSpeed;
+    private bool _hitBase = false;
 
 	// Use this for initialization
+
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 movement = new Vector2(1, 0f);
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+
+        if (_hitBase == false)
+        {
+            Vector2 movement = new Vector2(1, 0f);
+            transform.Translate(movement * _moveSpeed * Time.deltaTime);
+        }
+
 	
 	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Base")
+        {
+            Destroy(this.gameObject);
+            _hitBase = true;
+        }
+    }
 }
