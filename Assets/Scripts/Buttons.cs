6d90d1a8a70cdf5c92bb3b4db02a121fa8fa3902
bennie12345 <bmuijldert@gmail.com>
2015-11-24 10:38:25 +0100

    enemies now deal damage over time and added menu

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlayMainMenu()
+    {
+        Application.LoadLevel("MainScene");
+    }
+
+    public void InstructionsMainMenu()
+    {
+        Application.LoadLevel("InstructionScene");
+    }
+
+    public void QuitMainMenu()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    { 
+        Application.LoadLevel("MainMenu");
+    }
+}
---
+++ Assets/Scripts/RhinoEnemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RhinoEnemy : EnemyMovement
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+}
